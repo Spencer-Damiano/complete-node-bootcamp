a563991237a97693e3552d160d0870a4a3979e9b
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2025-12-03 21:32:02 -0700

    S2.10
    Did a couple of these videos instead of python work

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textOut = `this is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written successfully!');
+// const textOut = `this is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written successfully!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+            // Peak of call back hell, will learn about this later
+            fs.writeFile('./txt/final.txt', `${data1}\n${data3}`,'utf-8', err => {
+                console.log("Your file has been written :)")
+
+            });
+        });
+    });
+});
+
+console.log('Will read file!');
