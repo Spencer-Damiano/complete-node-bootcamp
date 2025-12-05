0e3f29d4ee5c725bd8a9230ebdd364508f82f7ec
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2025-12-04 21:32:19 -0700

    video for the day

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -8,19 +9,32 @@ const fs = require('fs');
 // console.log('File written successfully!');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
-            // Peak of call back hell, will learn about this later
-            fs.writeFile('./txt/final.txt', `${data1}\n${data3}`,'utf-8', err => {
-                console.log("Your file has been written :)")
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             // Peak of call back hell, will learn about this later
+//             fs.writeFile('./txt/final.txt', `${data1}\n${data3}`,'utf-8', err => {
+//                 console.log("Your file has been written :)")
 
-            });
-        });
-    });
+//             });
+//         });
+//     });
+// });
+
+// console.log('Will read file!');
+
+
+//////////////////////////////////
+// SERVER CODE
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    res.end('Hello from the server!');
 });
 
-console.log('Will read file!');
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
